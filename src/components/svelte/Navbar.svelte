330f17d4bb899dcf3ced53f5fe83e69b8b162967
Enrico Saez <enricoricsaez@gmail.com>
2024-09-04 16:23:49 -0300

    Instalações do projeto

---
+++ src/components/svelte/Navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<nav class="w-full px-4">
+	<div class="mx-auto flex h-14 max-w-6xl items-center justify-between">
+		<div></div>
+		<div class="*:nav-link flex space-x-8">
+			<a href="/membros">Membros</a>
+			<a href="/jogos">Jogos</a>
+			<a href="/seja-membro">Seja um membro</a>
+		</div>
+		<div>
+			<button class="bg-blue-700 px-4 py-2"> Login </button>
+		</div>
+	</div>
+</nav>
