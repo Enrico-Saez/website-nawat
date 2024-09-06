3891ea8d6e503f2e896c16ff22e9f61c3986c380
Enrico Saez <enricoricsaez@gmail.com>
2024-09-05 22:01:28 -0300

    Navbar, footer e parte da homepage

---
+++ src/components/svelte/NawatAreas.svelte
@@ -0,0 +1,125 @@
+<div class="mx-auto flex h-[50rem] max-w-5xl items-center justify-center">
+	<div>
+		<div class="text-center">
+			<h1 class="text-4xl font-black">Áreas de Desenvolvimento</h1>
+			<p>O desenvolvimento na Nawat é multi...</p>
+		</div>
+		<div class="mt-24 space-y-4">
+			<div
+				class="group h-[4.2rem] w-[28rem] overflow-hidden rounded-xl border border-blue-400 p-5 transition-all duration-300 hover:h-40"
+			>
+				<div class="flex items-center justify-between">
+					<span class="text-xl font-black">Game Design</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6 transition duration-300 group-hover:rotate-90"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+					</svg>
+				</div>
+				<p
+					class="mt-4 text-[0.925rem] opacity-0 transition delay-500 duration-500 group-hover:opacity-100"
+				>
+					Todos os membros que já passaram pela Nawat Games, contribuindo com suas ideias e seus
+					trabalhos.
+				</p>
+			</div>
+			<div
+				class="group h-[4.2rem] w-[28rem] overflow-hidden rounded-xl border border-green-400 p-5 transition-all duration-300 hover:h-40"
+			>
+				<div class="flex items-center justify-between">
+					<span class="text-xl font-black">Game Programming</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6 transition duration-300 group-hover:rotate-90"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+					</svg>
+				</div>
+				<p
+					class="mt-4 text-[0.925rem] opacity-0 transition delay-500 duration-500 group-hover:opacity-100"
+				>
+					Todos os membros que já passaram pela Nawat Games, contribuindo com suas ideias e seus
+					trabalhos.
+				</p>
+			</div>
+			<div
+				class="group h-[4.2rem] w-[28rem] overflow-hidden rounded-xl border border-rose-400 p-5 transition-all duration-300 hover:h-40 hover:bg-rose-500/10"
+			>
+				<div class="flex items-center justify-between">
+					<span class="text-xl font-black">Game Art</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6 transition duration-300 group-hover:rotate-90"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+					</svg>
+				</div>
+				<p
+					class="mt-4 text-[0.925rem] opacity-0 transition delay-500 duration-500 group-hover:opacity-100"
+				>
+					Todos os membros que já passaram pela Nawat Games, contribuindo com suas ideias e seus
+					trabalhos.
+				</p>
+			</div>
+			<div
+				class="group h-[4.2rem] w-[28rem] overflow-hidden rounded-xl border border-orange-400 p-5 transition-all duration-300 hover:h-40"
+			>
+				<div class="flex items-center justify-between">
+					<span class="text-xl font-black">Game Sound</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6 transition duration-300 group-hover:rotate-90"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+					</svg>
+				</div>
+				<p
+					class="mt-4 text-[0.925rem] opacity-0 transition delay-500 duration-500 group-hover:opacity-100"
+				>
+					Todos os membros que já passaram pela Nawat Games, contribuindo com suas ideias e seus
+					trabalhos.
+				</p>
+			</div>
+			<div
+				class="group h-[4.2rem] w-[28rem] overflow-hidden rounded-xl border border-fuchsia-400 p-5 transition-all duration-300 hover:h-40"
+			>
+				<div class="flex items-center justify-between">
+					<span class="text-xl font-black">Design Visual</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6 transition duration-300 group-hover:rotate-90"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+					</svg>
+				</div>
+				<p
+					class="mt-4 text-[0.925rem] opacity-0 transition delay-500 duration-500 group-hover:opacity-100"
+				>
+					Todos os membros que já passaram pela Nawat Games, contribuindo com suas ideias e seus
+					trabalhos.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,38 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
+const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				primary: '#36A9E1',
+				secondary: '#1e2388',
+				accent: '#543fd4',
+				background: '#081116',
+				text: '#ecf2f5',
+			},
 			fontFamily: {
 				sans: ['Inter', ...defaultTheme.fontFamily.sans],
 			},
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)',
+			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme('textShadow') }
+			)
+		}),
+	],
 }
